cfe05fe2c67c156c2a4a3c0cf7a38c27294d0b27
ezequielcarusela <ezequielromancarusela28@gmail.com>
2023-11-08 20:25:12 -0300

    Termine por hoy, agregue articulos al array y lo recorrí, tambien agregue una funcion constructora con prompt

--- js/main.js
+++ js/main.js
@@ -1,33 +1,77 @@
-function Articulo(tipo, nombre, precio, descripcion) {
-    this.tipo = tipo;
-    this.nombre = nombre;
-    this.precio = precio + " Pesos";
-    this.descripcion = descripcion;
+const articulos = [
+    {
+        tipo: "taza",
+        nombre: "Taza De Gryffindor",
+        precio: 3000,
+        descripcion: "Taza de ceramica de la casa de Harry Potter Gryffindor con capacidad de 250Ml"
+    },
+    {
+        tipo: "taza",
+        nombre: "Taza De Slytherin",
+        precio: 3000,
+        descripcion: "Taza de ceramica de la casa de Harry Potter Slytherin con capacidad de 250Ml"
+    },
+    {
+        tipo: "taza",
+        nombre: "Taza De Hufflepuff",
+        precio: 3000,
+        descripcion: "Taza de ceramica de la casa de Harry Potter Hufflepuff con capacidad de 250Ml"
+    },
+    {
+        tipo: "taza",
+        nombre: "Taza De Ravenclaw",
+        precio: 3000,
+        descripcion: "Taza de ceramica de la casa de Harry Potter Ravenclaw con capacidad de 250Ml"
+    },
+    {
+        tipo: "prenda",
+        nombre: "Remera de Gryffindor",
+        precio: 7000,
+        descripcion: "Remera hecha 100% de algodon con el logo de Gryffindor",
+    },
+    {
+        tipo: "prenda",
+        nombre: "Remera de Slytherin",
+        precio: 7000,
+        descripcion: "Remera hecha 100% de algodon con el logo de Slytherin",
+    },
+    {
+        tipo: "prenda",
+        nombre: "Remera de Hufflepuff",
+        precio: 7000,
+        descripcion: "Remera hecha 100% de algodon con el logo de Hufflepuff",
+    },
+    {
+        tipo: "prenda",
+        nombre: "Remera de Ravenclaw",
+        precio: 7000,
+        descripcion: "Remera hecha 100% de algodon con el logo de Ravenclaw",
+    },
+]
+
+
+
+
+function Articulo() {;
+    this.tipo = prompt("Que tipo de articulo quiere publicar");
+    this.nombre = prompt("Que tipo de articulo quiere publicar");
+    this.precio = prompt("Que tipo de articulo quiere publicar");
+    this.descripcion = prompt("Que tipo de articulo quiere publicar");
 }
-
-const item1 = new Articulo("Taza", "Taza de Gryffindor", 3000, "Taza de ceramica de la casa de Harry Potter Gryffindor con capacidad de 250Ml");
-
-const item2 = new Articulo("Taza", "Taza de Slytherin", 3000, "Taza de ceramica de la casa de Harry Potter Slytherin con capacidad de 250Ml")
-
-const item3 = new Articulo("Taza", "Taza de Ravenclaw", 3000, "Taza de ceramica de la casa de Harry Potter Ravenclaw con capacidad de 250Ml")
-
-const item4 = new Articulo("Taza", "Taza de Hufflepuff", 3000, "Taza de ceramica de la casa de Harry Potter Hufflepuff con capacidad de 250Ml")
-
-const item5 = new Articulo("Ropa", "Remera de Gryffindor", 6000, "Remera 100% algodon de Gryffindor")
-
-const item6 = new Articulo("Ropa", "Remera de Slytherin", 6000, "Remera 100% algodon de Slytherin")
-
-const item7 = new Articulo("Ropa", "Remera de Ravenclaw", 6000, "Remera 100% algodon de Ravenclaw")
-
-const item8 = new Articulo("Ropa", "Remera de Hufflepuff", 6000, "Remera 100% algodon de Hufflepuff")
-
-
-const articulos = [item1, item2, item3, item4, item5, item6, item7, item8]
-
-for (i=0;i<articulos.length;i+=1)
+function agregarArticulo(){
+    articulos.push()
+}
+agregarArticulo()
+for (let i=0;i<articulos.length;i+=1)
 
 console.log(articulos[i])
+// console.log(agregarArticulo)
 
-const filtrarArticulos = articulos.filter((articulo) => articulo.tipo("Taza"))
+// let filtrarTazas = articulos.filter(articulo => articulo.tipo === "taza")
 
-console.log(filtrar)
+// let filtrarPrendas = articulos.filter(articulo => articulo.tipo === "prenda")
+
+
+
+// console.log(filtrarTazas)
+// console.log(filtrarPrendas)
